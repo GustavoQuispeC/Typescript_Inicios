df3283b354b7d19be41fa0d0caee7727c8256529
Gustavo <gusstavocta@gmail.com>
2024-08-03 23:10:29 -0500

    Homework

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import './topics/01_basic_types';
 import './topics/02_object-interfaces'
 import './topics/03_Function'
+import './topics/04_homework-types'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
--- src/topics/03_Function.ts
+++ src/topics/03_Function.ts
@@ -1,21 +1,40 @@
-function addNumbers(a : number, b:number){
-    return a + b;
+function addNumbers(a: number, b: number) {
+  return a + b;
 }
-const result: number = addNumbers(1,2)
+const result: number = addNumbers(1, 2);
 
+const AddNumero = (a: number, b: number): string => {
+  return `${a + b}`;
+};
+const valor = AddNumero(2, 3);
 
-const AddNumero=(a:number, b: number):string =>{
-    return `${a + b}`;
+function multiply(firstNumer: number, seconNumber?: number, base: number = 2) {
+  return firstNumer * base;
 }
-const valor = AddNumero(2,3);
+const multiplyResult: number = multiply(5);
 
+console.log({ result, valor, multiplyResult });
 
-function multiply(firstNumer:number, seconNumber?:number, base:number = 2){
-    return firstNumer * base;
+interface Character {
+  name: string;
+  hp: number;
+  showHp: () => void;
 }
-const multiplyResult:number = multiply(5);
 
-console.log({result,valor,multiplyResult});
+const healCharacter = (character: Character, amount: number) => {
+  character.hp += amount;
+};
+const strider: Character = {
+  name: "Strider",
+  hp: 50,
+  showHp() {
+    console.log(`Puntos de vida ${this.hp}`);
+  },
+};
 
+healCharacter(strider, 20);
+healCharacter(strider, 30);
 
-export{}
+strider.showHp();
+
+export {};
